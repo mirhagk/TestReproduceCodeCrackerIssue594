f7075ca698876507d0cdcf3c40d6b413fd6817e9
Nathan Jervis <mirhagk@gmail.com>
2015-11-18 18:08:38 -0500

    Initial commit of successfully reproduced issue

---
+++ TestReproduceCodeCrackerIssue594/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestReproduceCodeCrackerIssue594
+{
+    class Program
+    {
+        class ChangePasswordMessageId
+        {
+        }
+        static string AlterPassword(ChangePasswordMessageId message)
+        {
+            if (message != null)
+                return "";
+            return "AlterPassword";
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
